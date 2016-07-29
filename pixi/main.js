0b9daaf1d5b9f367661aba454873c5da3ba7e9fc
minimo <minimo.mobile@gmail.com>
2016-07-29 13:19:47 +0900

    Update sample

--- pixi/main.js
+++ pixi/main.js
@@ -1,50 +1,93 @@
-var SC_W = 800;
+/*
+
+  phina.js と pixi.ja 連携テスト
+
+*/
+
+var SC_W = 640;
 var SC_H = 640;
+
+//テストするスプライトのタイプ "phina" or "pixi"
+var testType = "pixi";
 
 phina.define('MainScene', {
   superClass: 'phina.display.DisplayScene',
   init: function() {
     this.superInit({width:SC_W, height: SC_H});
 
+    this.phinaLayer = phina.display.DisplayElement().addChildTo(this);
     this.pixiLayer = phina.display.PixiLayer({width: SC_W, height: SC_H}).addChildTo(this);
 
+    var that = this;
+    phina.display.Label()
+      .setPosition(SC_W*0.5, 20)
+      .addChildTo(this)
+      .update = function() {
+        this.text = "tomapiko: "+ (that.pixiLayer.children.length + that.phinaLayer.children.length);
+      }
+
     //Tomapiko読み込み
+    this.finish = false;
     var that = this;
     var url = "https://rawgit.com/phi-jp/phina.js/develop/assets/images/tomapiko_ss.png";
-    var texture = phina.asset.Texture();
-    texture.load(url).then(function() {
-//        phina.display.Sprite(texture).addChildTo(that).setOrigin(0,0);
-
-        that.tomapiko = phina.display.Sprite(texture, 64, 64)
-          .addChildTo(that)
-          .setFrameTrimming(192, 128, 192, 64)
-          .setFrameIndex(0)
-          .setPosition(SC_W*0.1, SC_H*0.1);
-        that.tomapiko.update = function(e) {
-          this.frameIndex++;
-        }
-
-        var sprite = phina.display.PixiSprite(texture, 64, 64)
-          .addChildTo(that.pixiLayer)
-          .setPosition(SC_W*0.5, SC_H*0.5)
-          .setFrameTrimming(192, 128, 192, 64)
-          .setFrameIndex(0);
-        sprite.update = function(e) {
-          this.frameIndex++;
-        }
-
+    this.texture = phina.asset.Texture();
+    this.texture.load(url).then(function() {
+      for (var i = 0; i < 10; i++) {
+        that.addSprite(testType);
+      }
+      this.finish = true;
     }.bind(this));
+  },
+
+  update: function() {
+    if (!this.finish) return;
+
+    var p = app.pointer;
+    if (p.getPointing()) {
+      for (var i = 0; i < 10; i++) {
+        this.addSprite(testType, p);
+      }
+    }
+  },
+
+  addSprite: function(type, position) {
+    position = position || {x: SC_W*0.5, y: SC_H*0.5};
+    var sp;
+    if (type == "phina") {
+      sp = phina.display.Sprite(this.texture, 64, 64)
+        .addChildTo(this.phinaLayer)
+        .setFrameTrimming(192, 128, 192, 64)
+        .setFrameIndex(0)
+        .setPosition(position.x, position.y);
+    } else {
+      sp = phina.display.PixiSprite(this.texture, 64, 64)
+        .addChildTo(this.pixiLayer)
+        .setFrameTrimming(192, 128, 192, 64)
+        .setFrameIndex(0)
+        .setPosition(position.x, position.y);
+    }
+    sp.vec = phina.geom.Vector2(Math.randint(-10, 10), Math.randint(-10, 10));
+    sp.alpha = Math.randfloat(0.2, 1.0);
+    sp.update = function(e) {
+      this.frameIndex++;
+      this.rotation++;
+      this.position.add(this.vec);
+      if (this.x < 0 || this.x > SC_W) this.vec.x *= -1;
+      if (this.y < 0 || this.y > SC_H) this.vec.y *= -1;
+    }
+    return sp;
   },
 });
 
 phina.main(function() {
-  var app = phina.game.GameApp({
+  app = phina.game.GameApp({
     startLabel: 'main',
     width: SC_W,
     height: SC_H,
     backgroundColor: "#ccc",
   });
   app.fps = 60;
+  app.enableStats();
   app.run();
 });
 
